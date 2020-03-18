16b92736ea544054f3b1d031f94cc21b3d7a95ad
yifan-fanyi <yifanwang0916@outlook.com>
2020-03-18 16:36:52 -0700

    Update myRegression.py
    better mapping function, stop using predict_proba

--- myRegression.py
+++ myRegression.py
@@ -1,10 +1,10 @@
-# 2020.02.15 
+# 2020.03.18
 #
 # regression on subspace
 # particular designed for encounter missing class in this subspace
 # if one class do not exists in training data, probability for this class would be zeros under anytime
 #
-#   regressor, a regressor or classifier, must have methods named 'predict' or 'predict_proba'
+#   learner, a regressor or classifier, must have methods named 'predict'
 #   num_class: total number of class in dataset
 
 import numpy as np 
@@ -13,60 +13,51 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class myRegression():
-    def __init__(self, regressor, num_class):
-        self.regressor = regressor
+    def __init__(self, learner, num_class):
+        self.learner = learner
         self.num_class = num_class
-        self.class_list = np.zeros((num_class))
+        self.class_list = {}
+    
+    def mapping(self, Y, train=True):
+        res = []
+        Y = Y.reshape(-1)
+        if train == True:
+            c = 0
+            for i in range(np.array(Y).shape[0]):
+                if Y[i] not in self.class_list:
+                    self.class_list[Y[i]] = c
+                    c+=1
+                res.append(self.class_list[Y[i]])
+        else:
+            for i in range(np.array(Y).shape[0]):
+                for d in self.class_list.keys():
+                    if self.class_list[d] == Y[i]:
+                        res.append(d)
+        return np.array(res)
 
     def fit(self, X, Y):
-        tmp = np.unique(Y)
-        tmp = tmp.astype('int32')
-        if tmp.shape[0] == self.num_class:
-            self.class_list = np.ones((self.num_class))
-        elif tmp.shape[0] == 1:
-            self.class_list[tmp[0]] = 1
-            return
-        else:
-            for i in range(tmp.shape[0]):
-                Y[Y == tmp[i]] = i
-                self.class_list[tmp[i]] = 1
-        self.regressor.fit(X, Y)
+        Y = self.mapping(Y, train=True)
+        self.learner.fit(X, Y)
 
-    def predict_proba(self, X):
-        if np.sum(self.class_list) == 1:
-            return np.ones((X.shape[0], self.num_class)) * self.class_list
-        try:
-            tmp_pred = self.regressor.predict_proba(X)
-        except:
-            tmp_pred = self.regressor.predict(X).reshape(-1,1)
-            tmp_pred = np.concatenate((tmp_pred, 1-tmp_pred), axis=1)
-        pred = np.zeros((X.shape[0], self.num_class))
-        idx = 0
-        for i in range(self.num_class):
-            if self.class_list[i] == 1:
-                pred[:, i] = tmp_pred[:, idx]
-                idx += 1
-        return pred.reshape(X.shape[0], self.num_class)
+    def predict(self, X):    
+        tmp_pred = self.learner.predict(X).reshape(-1)
+        pred= self.mapping(tmp_pred,  train=False)
+        return pred
 
     def score(self, X, Y):
-        try:
-            score = self.regressor.score(X, Y)
-        except:
-            pred = self.predict_proba(X)
-            pred = np.argmax(pred, axis=1)
-            score = accuracy_score(pred, Y)
-        #print("           <Debug Info>        accuracy on node: %s"%str(score))
+        return self.learner.score(X, Y) 
 
 if __name__ == "__main__":
-    import cv2
-    X = cv2.imread('./data/test.jpg')
-    X = cv2.resize(X, (40,40))
-    X = X.reshape(-1,3)
-    Y = np.random.randint(3, size=X.shape[0]) + 1
-    Y = Y.reshape(-1,1)
     from sklearn.linear_model import LogisticRegression
-    tmp = LogisticRegression(random_state=0, solver='liblinear', multi_class='ovr', n_jobs=20, max_iter=1000).fit(X, Y.reshape(-1,))
-    reg = myRegression(tmp, 5)
-    reg.fit(X, Y)
-    pred = reg.predict_proba(X)
-    print(" \n--> Regression result... \n", pred)
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+    print(" \n> This is a test enample using MNIST: ")
+    digits = datasets.load_digits()
+    X = digits.images.reshape((len(digits.images), -1))
+    X_train, X_test, y_train, y_test = train_test_split(X, digits.target, test_size=0.5, shuffle=False)
+    clf = myRegression(LogisticRegression(random_state=0, solver='liblinear', multi_class='ovr', n_jobs=20, max_iter=1000),
+             10)
+    clf.fit(X_train, y_train)
+    print(" --> train acc: %s"%str(clf.score(X_train, y_train)))
+    print(" --> test acc.: %s"%str(clf.score(X_test, y_test)))
+    print("------- DONE -------\n")
