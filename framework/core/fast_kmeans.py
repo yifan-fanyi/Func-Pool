9d0a293f90b19519b3f9cdc46425cbc22385ec62
yifan-fanyi <yifanwang0916@outlook.com>
2021-04-27 12:40:34 -0700

    gpu KMeans using faiss, detail see framework/core/fast_kmeans.py

--- framework/core/fast_kmeans.py
+++ framework/core/fast_kmeans.py
@@ -1,27 +1,42 @@
 # 2021.04.23
 # https://www.kdnuggets.com/2021/01/k-means-faster-lower-error-scikit-learn.html
 # faster kmeans
+# gpu is supported
+# conda install -c conda-forge faiss-gpu
+#
 import faiss
 import numpy as np
 
 class fast_KMeans:
-    def __init__(self, n_clusters=8, n_init=10, max_iter=300):
+    def __init__(self, n_clusters=8, n_init=10, max_iter=300, gpu=False):
         self.n_clusters = n_clusters
         self.n_init = n_init
         self.max_iter = max_iter
         self.kmeans = None
         self.cluster_centers_ = None
         self.inertia_ = None
+        self.gpu = gpu            
         self.__version__ = faiss.__version__
 
     def fit(self, X):
-        self.kmeans = faiss.Kmeans(d=X.shape[1],
-                                   k=self.n_clusters,
-                                   niter=self.max_iter,
-                                   nredo=self.n_init)
-        self.kmeans.train(X.astype(np.float32))
+        if self.gpu != False:
+            self.kmeans = faiss.Kmeans(d=X.shape[1],
+                                    k=self.n_clusters,
+                                    niter=self.max_iter,
+                                    nredo=self.n_init,
+                                    gpu=self.gpu,
+                                    )
+        else:
+            self.kmeans = faiss.Kmeans(d=X.shape[1],
+                                    k=self.n_clusters,
+                                    niter=self.max_iter,
+                                    nredo=self.n_init,
+                                    )
+        X = np.ascontiguousarray(X)
+        self.kmeans.train(X)
         self.cluster_centers_ = self.kmeans.centroids
         self.inertia_ = self.kmeans.obj[-1]
 
     def predict(self, X):
+        X = np.ascontiguousarray(X)
         return self.kmeans.index.search(X.astype(np.float32), 1)[1]
